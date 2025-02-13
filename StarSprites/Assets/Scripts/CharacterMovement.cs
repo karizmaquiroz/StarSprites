403587d8cff59658d7320fabd6faaabce7d5bc1a
lismar17 <lisbeth.mtzj@gmail.com>
2025-02-13 13:37:42 -0500

    Animated
    ANIMATIONS MADEE!!

--- StarSprites/Assets/Scripts/CharacterMovement.cs
+++ StarSprites/Assets/Scripts/CharacterMovement.cs
@@ -12,15 +12,31 @@ public class CharacterMovement : MonoBehaviour
     private int jumpCount = 0;
     private int maxJumps = 2;
 
+   //animator (lis)
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
+        animator.SetFloat("walking", Mathf.Abs(moveInput));
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (moveInput > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
@@ -33,6 +49,8 @@ public class CharacterMovement : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             jumpCount++;
+            animator.SetTrigger("jumping");
+
         }
     }
-}
+ }
