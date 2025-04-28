97249b2b0e8a1261c747b64709e72f8088c6488a
celestewish <laurenrousell@outlook.com>
2025-04-28 19:33:10 -0400

    Updates to HUD

--- StarSprites/Assets/Scripts/UI/PauseMenu.cs
+++ StarSprites/Assets/Scripts/UI/PauseMenu.cs
@@ -11,43 +11,24 @@ public class PauseMenu : MonoBehaviour
 
     private void Awake()
     {
-        PausePanel.SetActive(false);
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F10))
-        {
-            if (GameIsPaused) { Resume(); }
-            else { Pause(); }
-        }
-       
-    }
-
-    public void restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        Time.timeScale = 1f;
-        //Debug.Log("Playing game / inside game);
-    }
-
-    public void LoadMainMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Main Menu");
-        //Debug.Log("In the MainMenu scene");
+        if (PausePanel != null)
+            PausePanel.SetActive(false);
     }
 
     public void Pause()
     {
-        Time.timeScale = 0f;
-        PausePanel.SetActive(true);
+        Debug.Log("Pause button pressed!");
+        Time.timeScale = 0f; // Freeze time
+        if (PausePanel != null)
+            PausePanel.SetActive(true);
         GameIsPaused = true;
     }
 
     public void Resume()
     {
-        Time.timeScale = 1f;
-        PausePanel.SetActive(false);
-        GameIsPaused = true; //changed
+        Time.timeScale = 1f; // Unfreeze time
+        if (PausePanel != null)
+            PausePanel.SetActive(false);
+        GameIsPaused = false;
     }
 }
